05e97aa5757ae5476ebdd7700fd16dc2822e6285
anvilleg <50292334+Apaulav@users.noreply.github.com>
2024-06-14 08:00:55 +0200

    cambio en la conexión con la base de datos

--- 5_todoApp/todo-app-backend-Apaulav/app.js
+++ 5_todoApp/todo-app-backend-Apaulav/app.js
@@ -2,14 +2,14 @@ const express = require("express");
 const app = express();
 const cors = require('cors')
 const port = 3000;
+require('dotenv').config()
 
 const taskRoute =require("./routes/taskRoute");
 const userRoute =require("./routes/userRoute");
 
 const mongoose = require("mongoose");
 require('dotenv').config();
-//const mongoDB = "mongodb+srv://"+process.env.DB_USER+":"+process.env.DB_PASSWORD+"@"+process.env.DB_SERVER+"/"+process.env.DB_NAME+"?retryWrites=true&w=majority";
-const mongoDB = "mongodb+srv://mongodbuiifhjskliw:otiFvN1ov1h7rna9@cluster0.1cslvrx.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+const mongoDB = "mongodb+srv://"+process.env.DB_USER+":"+process.env.DB_PASSWORD+"@"+process.env.DB_SERVER+"/"+process.env.DB_NAME+"?retryWrites=true&w=majority";
 async function main() {
   await mongoose.connect(mongoDB);
   //console.log('Conectado con la Base de datos')
